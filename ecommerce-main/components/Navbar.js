93c7a73928c1a47211cc4f41f64451a53f186400
Shuvam-Chakraborty <shuvamchakraborty2002@gmail.com>
2022-10-26 00:56:11 +0530

    ecommerce main

---
+++ ecommerce-main/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai';
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities, user, admin } =
+    useStateContext();
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>Captcha Headphones</Link>
+      </p>
+      <p className='logo'>
+        <Link href='/login'>Login</Link>
+      </p>
+      <p className='logo'>
+        <Link href='/register'>Register</Link>
+      </p>
+      <p className='logo'>
+        <Link href='/admin-login'>Admin Login</Link>
+      </p>
+
+      <button
+        type='button'
+        className='cart-icon'
+        onClick={() => {
+          setShowCart(!showCart);
+        }}
+      >
+        <AiOutlineUser />
+        {user}
+        <AiOutlineShoppingCart />
+        <span className='cart-item-qty'>{totalQuantities}</span>
+      </button>
+      {showCart && <Cart />}
+    </div>
+  );
+};
+
+export default Navbar;
